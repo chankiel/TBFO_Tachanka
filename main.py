110f9025c4eaa903019f09d6b9a750dc14676c26
Ignatius Jhon Hezkiel Chan <kielchanyeol@gmail.com>
2023-11-26 11:08:08 +0700

    feat: add main.py and alr running checker

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Jika parser membuang
+import parser
 totalStates = []
 inputSymbols = []
 stackSymbols = []
@@ -18,13 +18,13 @@ for pdaline in contentpdaline:
     elif linecount == 3:
         stackSymbols = pdaline.strip().split()
     elif linecount == 4:
-        currentState = pdaline.strip().split()
+        currentState = (pdaline.strip().split())[0]
     elif linecount == 5:
-        pdaStack.append(pdaline.strip().split())
+        pdaStack.append((pdaline.strip().split())[0])
     elif linecount == 6:
-        acceptingState = pdaline.strip().split()
+        acceptingState = (pdaline.strip().split())[0]
     elif linecount == 7:
-        acceptingMethod = pdaline.strip().split()
+        acceptingMethod = (pdaline.strip().split())[0]
     else:
         arr = pdaline.strip().split()
         key = (arr[0],arr[1],arr[2])
@@ -37,19 +37,38 @@ for pdaline in contentpdaline:
         pdaRules[key] = value
     linecount += 1
 
-arr = parser("temp.txt")
+# for key, value in pdaRules.items():
+#     print(f"Key: {key}, Value: {value}")
 
+arr = parser.parser("index.html")
+print(arr)
 for val in arr:
+    if(val==""):
+        continue
     top = pdaStack.pop()
+    # print("CurrentState : "+currentState)
+    # print("input :",val)
+    # print("top stack :",top)
     res = pdaRules.get((currentState,val,top),("NO_STATE","NO_TOP"))
     if(res == ("NO_STATE","NO_TOP")):
         res = pdaRules.get((currentState,val,"<X>"),("NO_STATE","NO_TOP"))
-    if(res == ("NO_STATE","NO_TOP")):
+    if(res == ("NO_STATE","NO_TOP") or res==("ERROR","e")):
+        print(currentState)
+        print(val)
+        print(top)
         break
     currentState = res[0]
+    # print("After currentState:",currentState)
+    # print("Next Top", res[1])
     for elmt in reversed(res[1]):
-        pdaStack.append(elmt)
-    
+        if(elmt!="" and elmt!='e'):
+            if(elmt=='<X>'):
+                # print("Elmt append stack:",top)
+                pdaStack.append(top)
+            else:
+                # print("Elmt append stack:",elmt)
+                pdaStack.append(elmt)
+
 if(currentState==acceptingState):
     print("Accepted\n")
 else:
---
+++ parser.py
@@ -0,0 +1,107 @@
+def remove(s, indx):
+    s = list(s)
+    s.pop(indx)
+    return ''.join(s)
+# test
+# str = "abcdef"
+# print(str)
+# str = remove(str,-2)
+# print(str)
+
+def parser(filename):
+    strFile = ""
+    listFile = []
+    listFile2 = []
+    listFinal = []
+    tempTag = ""
+    count = 0
+    tempAtt = ""
+    with open(filename, 'r',encoding='utf-8') as file:
+        # Baca file
+        content = file.read()
+        strFile = content
+    strFile=strFile.replace('\n', '')
+
+    for char in strFile:
+        if(char == ">"):
+            tempTag += ">"
+            listFile.append(tempTag)
+            tempTag = ""
+        elif(char == "<"):
+            if(len(tempTag) == 0):
+                tempTag += "<"
+            else:
+                if(tempTag[0] != "<"):
+                    listFile.append(tempTag)
+                    tempTag = "<"
+                else:
+                    tempTag += "<"
+        elif(char == " "):
+            x = len(tempTag)
+            if(x != 0):
+                if(tempTag[-1] == " "):
+                    pass
+                else:
+                    tempTag += char
+        else:
+            tempTag += char
+
+    # clear whitespace behind ">"
+    for i in range(len(listFile)):
+        elmt = listFile[i]
+        if(len(elmt) > 1):
+            if(elmt[0] == "<"):
+                if(elmt[-2] == " "):
+                    elmt = remove(elmt,-2)
+                    listFile[i] = elmt
+    for k in range(len(listFile)):
+        elmt = listFile[k]
+        if("<" == elmt[0] and ">" == elmt[-1]):
+            listFile2.append(elmt)
+    # dekomposisi open tag with att
+    for j in range(len(listFile2)):
+        elmt = listFile2[j]
+        if(len(elmt) <= 1):
+            listFinal.append(elmt)
+        else:
+            if(elmt[0] != "<"):
+                listFinal.append(elmt)
+            else:
+                if(elmt[1] == "/"):
+                    listFinal.append(elmt)
+                    
+                else:
+                    temp = list(elmt)
+                    hasil = ""
+                    for cc in temp:
+                        if(cc == " "):
+                            if(len(hasil) != 0):
+                                if(hasil[0] == "<"):
+                                    listFinal.append(hasil)
+                                    hasil = ""
+                                elif(hasil[-1] == "="):
+                                    pass
+                                elif(len(hasil)>1 and hasil[-1] == "\"" and hasil[-2] == "\""):
+                                    listFinal.append(hasil)
+                                    hasil = ""
+                        elif(cc == ">"):
+                            listFinal.append(hasil)
+                            listFinal.append(">")
+                            hasil = ""
+
+                        else:
+                            if(len(hasil) != 0):
+                                if("\"" in hasil):
+                                    if(cc == "\""):
+                                        hasil += cc
+                                        listFinal.append(hasil)
+                                        hasil = ""
+                                    elif("type=\"" in hasil or "method=\"" in hasil):
+                                        hasil += cc
+                                    else:
+                                        pass
+                                else:    
+                                    hasil += cc
+                            else:         
+                                hasil += cc
+    return listFinal
